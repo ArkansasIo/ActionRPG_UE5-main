2b188527faf48a6985249b3ed04e35b3868242cb
stephen <95516724+ArkansasIo@users.noreply.github.com>
2026-02-09 19:42:50 -0500

    Upgrade build settings to BuildSettingsVersion.V6 for Unreal Engine 5.4 compatibility

--- Action-RPG-main/Source/Warrior.Target.cs
+++ Action-RPG-main/Source/Warrior.Target.cs
@@ -8,7 +8,7 @@ public class WarriorTarget : TargetRules
 	public WarriorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("Warrior");
 	}
